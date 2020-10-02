4ef336a7bb16949ee88e06a6f89cde2674d5447a
Roby Ibrahim Renata <roby.renata@codify.id>
2020-10-02 15:17:04 +0700

    [INIT] update auth service

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Observable } from 'rxjs';
+import { CacheService } from './cache.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private jwtHelper: JwtHelperService, private cache: CacheService) {}
 
-  constructor() { }
+  checkToken() {
+    this.cache.getToken().then((token) => {
+      if (token) {
+        const decoded = this.jwtHelper.decodeToken(token);
+        const isExpired = this.jwtHelper.isTokenExpired(token);
+
+        !isExpired ? this.cache.setCurrentUser(decoded, token) : this.cache.removeToken();
+      }
+    });
+  }
+
+  isAuthenticated() {
+    return new Observable<boolean>((observer) => {
+      this.cache.getToken().then((token) => {
+        if (token) {
+          const decoded = this.jwtHelper.decodeToken(token);
+          const isExpired = this.jwtHelper.isTokenExpired(token);
+          if (!isExpired) {
+            this.cache.setCurrentUser(decoded, token);
+            observer.next(true);
+          } else {
+            this.cache.removeToken();
+            observer.next(false);
+          }
+        } else {
+          observer.next(false);
+        }
+      });
+    });
+  }
 }
